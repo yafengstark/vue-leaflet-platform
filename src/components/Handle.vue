d4ea98aa8c6c296d32c0e3ae7e45bcfa75d31735
fengfeng043 <729964264@qq.com>
2019-09-21 11:55:28 +0800

    将标注检索抽离成子组件

--- src/components/Handle.vue
+++ src/components/Handle.vue
@@ -3,9 +3,11 @@
         <!--  操作面板 -->
 
         <div class="button-container">
-            <Button type="text" @click="createMark">
-                <Icon type="md-create" size="28"/>
-                <br/>新建
+            <Button type="text" @click="">
+                <router-link to="/mark/new" target="_blank">
+                    <Icon type="md-create" size="28"/>
+                    <br/>新建
+                </router-link>
             </Button>
             <!--<Button type="text" @click="editMark">-->
             <!--<Icon type="md-create" size="28"/>-->
@@ -13,10 +15,7 @@
             <!--</Button>-->
 
             <!--TODO: 抽屉实现搜索-->
-            <Button type="text" @click="value3 = true">
-                <Icon type="md-search" size="28"/>
-                <br/>搜索
-            </Button>
+            <search></search>
 
             <Button type="text">
                 <Icon type="md-download" size="28"/>
@@ -24,84 +23,7 @@
             </Button>
 
 
-            <Drawer
-                    title="搜索"
-                    v-model="value3"
-                    width="720"
-                    :mask-closable="false"
-                    :styles="styles"
-            >
-                <Form :model="formData">
-                    <Row :gutter="32">
-                        <Col span="12">
-                        <FormItem label="名称" label-position="top">
-                            <Input v-model="formData.name" placeholder="请输入名称"/>
-                        </FormItem>
-                        </Col>
-                        <Col span="12">
-                        <FormItem label="位置" label-position="top">
-                            <label>最小经度：</label><Input class="input" v-model="myMapHandleObject.rectangle.minLon"
-                                                       placeholder="120" style="width: 300px"/>
-                            <label>最大经度：</label><Input class="input" v-model="myMapHandleObject.rectangle.maxLon"
-                                                       placeholder="120" style="width: 300px"/>
-                            <label>最小纬度：</label><Input class="input" v-model="myMapHandleObject.rectangle.minLat"
-                                                       placeholder="120" style="width: 300px"/>
-                            <label>最大纬度：</label><Input class="input" v-model="myMapHandleObject.rectangle.maxLat"
-                                                       placeholder="120" style="width: 300px"/>
-                        </FormItem>
-                        </Col>
-                        <Col span="12">
-                        <FormItem label="Url" label-position="top">
-                            <Input v-model="formData.url" placeholder="please enter url">
-                            <span slot="prepend">http://</span>
-                            <span slot="append">.com</span>
-                            </Input>
-                        </FormItem>
-                        </Col>
-                    </Row>
-                    <Row :gutter="32">
-                        <Col span="12">
-                        <FormItem label="Owner" label-position="top">
-                            <Select v-model="formData.owner" placeholder="please select an owner">
-                                <Option value="jobs">Steven Paul Jobs</Option>
-                                <Option value="ive">Sir Jonathan Paul Ive</Option>
-                            </Select>
-                        </FormItem>
-                        </Col>
-                        <Col span="12">
-                        <FormItem label="Type" label-position="top">
-                            <Select v-model="formData.type" placeholder="please choose the type">
-                                <Option value="private">Private</Option>
-                                <Option value="public">Public</Option>
-                            </Select>
-                        </FormItem>
-                        </Col>
-                    </Row>
-                    <Row :gutter="32">
-                        <Col span="12">
-                        <FormItem label="Approver" label-position="top">
-                            <Select v-model="formData.approver" placeholder="please choose the approver">
-                                <Option value="jobs">Steven Paul Jobs</Option>
-                                <Option value="ive">Sir Jonathan Paul Ive</Option>
-                            </Select>
-                        </FormItem>
-                        </Col>
-                        <Col span="12">
-                        <FormItem label="DateTime" label-position="top">
-                            <DatePicker v-model="formData.date" type="daterange" placeholder="please select the date"
-                                        style="display: block" placement="bottom-end"></DatePicker>
-                        </FormItem>
-                        </Col>
-                    </Row>
-                    <FormItem label="描述" label-position="top">
-                        <Input type="textarea" v-model="formData.desc" :rows="4" placeholder="请输入描述关键字"/>
-                    </FormItem>
-                </Form>
-                <div class="demo-drawer-footer">
-                    <Button style="margin-right: 8px" @click="value3 = false">Cancel</Button>
-                    <Button type="primary" @click="value3 = false">Submit</Button>
-                </div>
-            </Drawer>
+
 
 
         </div>
@@ -114,28 +36,14 @@
     // 导入自己封装的轮播图子组件
 
     import MyBackTop from './MyBackTop.vue'
+    import Search from './mark/Search.vue'
+
     import {mapActions, mapState} from 'vuex'
 
     export default {
         data() {
             return {
-                value3: false,
-                styles: {
-                    height: 'calc(100% - 55px)',
-                    overflow: 'auto',
-                    paddingBottom: '53px',
-                    position: 'static'
-                },
-                formData: {
-                    name: '',
-                    url: '',
-                    owner: '',
-                    type: '',
-                    approver: '',
-                    date: '',
-                    desc: '',
-//                    rectangle: this.myMapHandleObject.rectangle,
-                },
+
             };
         },
         computed: {
@@ -158,7 +66,8 @@
         },
         components: {
             // 注册子组件
-            MyBackTop
+            MyBackTop,
+            Search
 
         }
     };
